518dd821941be996cd3da5681bb3c6081cf28d6b
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-07-18 11:47:17 +0300

    Constraints in department DTOs

--- Application/DTOs/Department/CreateDepartmentDTO.cs
+++ Application/DTOs/Department/CreateDepartmentDTO.cs
@@ -6,6 +6,7 @@ namespace Application.DTOs.Department
     public class CreateDepartmentDTO
     {
         [Required(ErrorMessage = "Name cannot be empty")]
+        [StringLength(50, ErrorMessage = "Department Name cannot exceed 50 characters.")]
         [Display(Name = "Name")]
         public string Name { get; set; } = null!;
 
