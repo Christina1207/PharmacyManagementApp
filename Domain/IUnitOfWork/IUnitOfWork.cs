a6cfb3ca4b4b7249de162b431b3b8016a92ebf2d
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-07-24 14:07:55 +0300

    Unit Of Work Pattern

---
+++ Domain/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.IUnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Prescription, int> Prescriptions { get; }
+        IRepository<InventoryItemDetail, int> InventoryItemDetails { get; }
+        IRepository<Sale, int> Sales { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Domain.IRepositories;
+using Domain.IUnitOfWork;
+using Infrastructure.Context;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork :IUnitOfWork
+    {
+        private readonly PharmacyDbContext _context;
+        public IRepository<Prescription, int> Prescriptions { get; private set; }
+        public IRepository<InventoryItemDetail, int> InventoryItemDetails { get; private set; }
+        public IRepository<Sale, int> Sales { get; private set; }
+
+        public UnitOfWork(PharmacyDbContext context)
+        {
+            _context = context;
+            Prescriptions = new Repository<Prescription, int>(_context);
+            InventoryItemDetails = new Repository<InventoryItemDetail, int>(_context);
+            Sales = new Repository<Sale, int>(_context);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
