7daed5b4fe54052d6890569c2fed2fb1f1823439
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-08-02 23:02:34 +0300

    Refactor Patient and doctor endpoints

---
+++ PharmacyManagmentApp/Controllers/DoctorsController.cs
@@ -0,0 +1,167 @@
+﻿using Application.DTOs.Doctor;
+using Application.IServices.Doctor;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PharmacyManagmentApp.Controllers
+{
+    [Authorize(Roles = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/admin/doctors")]
+    public class DoctorsController : Controller
+    {
+        private readonly IDoctorService _doctorService;
+        public DoctorsController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+        // * Doctors * //
+
+        [HttpGet]
+        public async Task<IActionResult> GetDoctors()
+        {
+
+            try
+            {
+                var result = await _doctorService.GetAllDoctorsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Failed to retrieve doctors",
+                    Details = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDoctorById(int id)
+        {
+            try
+            {
+                var dep = await _doctorService.GetDoctorByIdAsync(id);
+                if (dep == null) return NotFound(new { Error = $"Doctor with ID {id} not found" });
+                return Ok(dep);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = $"An error occurred while retrieving doctor with ID {id}",
+                    Details = ex.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor([FromBody] CreateDoctorDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Error = "Validation failed",
+                    Details = ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage)
+                });
+            }
+            try
+            {
+                var newDep = await _doctorService.CreateDoctorAsync(dto);
+                return CreatedAtAction(nameof(GetDoctorById), new { id = newDep.Id }, newDep);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Failed to create doctor",
+                    Details = ex.Message
+                });
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDoctor(int id, [FromBody] UpdateDoctorDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Error = "Validation failed",
+                    Details = ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage)
+                });
+            }
+            try
+            {
+                await _doctorService.UpdateDoctorAsync(dto);
+                return Ok(dto);
+            }
+            catch (KeyNotFoundException e)
+            {
+                return NotFound(new { Error = e.Message });
+
+            }
+            catch (DbUpdateException e)
+            {
+                return Conflict(new { Error = e.Message });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = $"An error occurred while updating doctor with ID {id}",
+                    Details = ex.Message
+                });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDoctor(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Error = "Validation failed",
+                    Details = ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage)
+                });
+            }
+            try
+            {
+                await _doctorService.DeleteDoctorAsync(id);
+                return Ok($"Deleted doctor with id {id}");
+            }
+            catch (KeyNotFoundException e)
+            {
+                return NotFound(new { Error = e.Message });
+
+            }
+            catch (DbUpdateException e)
+            {
+                return Conflict(new { Error = e.Message });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = $"An error occurred while Deleting Doctor with ID {id}",
+                    Details = ex.Message
+                });
+            }
+        }
+
+    }
+}
---
+++ PharmacyManagmentApp/Controllers/PatientsController.cs
@@ -0,0 +1,151 @@
+﻿using Application.DTOs.Employee;
+using Application.DTOs.FamilyMember;
+using Application.IServices.Employee;
+using Application.IServices.FamilyMember;
+using Application.IServices.InsuredPerson;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PharmacyManagmentApp.Controllers
+{
+    [Authorize(Roles = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/admin/patients")]
+    public class PatientsController : Controller
+    {
+        private readonly IInsuredPersonService _insuredPersonService;
+        private readonly IEmployeeService _employeeService;
+        private readonly IFamilyMemberService _familyMemberService;
+
+        public PatientsController(IInsuredPersonService insuredPersonService, IEmployeeService employeeService, IFamilyMemberService familyMemberService)
+        {
+            _insuredPersonService = insuredPersonService;
+            _employeeService = employeeService;
+            _familyMemberService = familyMemberService;
+         
+        }
+
+
+        [HttpGet] // GET /api/admin/patients
+        public async Task<IActionResult> GetAllPatients()
+        {
+            var patients = await _insuredPersonService.GetAllInsuredPersonsAsync();
+            return Ok(patients);
+        }
+
+        [HttpGet("{id}")]  // GET /api/admin/patients/{id}
+        public async Task<IActionResult> GetPatientById(int id)
+        {
+            try
+            {
+                var patient = await _insuredPersonService.GetInsuredPersonByIdAsync(id);
+                return Ok(patient);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { Message = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}/activate")]  // PUT /api/admin/patients/{id}/activate
+        public async Task<IActionResult> ActivatePatient(int id)
+        {
+            try
+            {
+                await _insuredPersonService.ActivateInsuredPersonAsync(id);
+                return Ok(new { Message = $"Patient with ID {id} has been activated." });
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { Message = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}/deactivate")] // PUT /api/admin/patients/{id}/deactivate
+        public async Task<IActionResult> DeactivatePatient(int id)
+        {
+            try
+            {
+                await _insuredPersonService.DeactivateInsuredPersonAsync(id);
+                return Ok(new { Message = $"Patient with ID {id} has been deactivated." });
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { Message = ex.Message });
+            }
+        }
+
+
+
+        /*  Employees  */
+
+        [HttpPost("employees/{employeeId}/familymembers")] // POST /api/admin/patients/employees/{employeeId}/familymembers
+        public async Task<IActionResult> AddFamilyMember(int employeeId, [FromBody] CreateFamilyMemberDTO dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                var newFamilyMember = await _familyMemberService.AddFamilyMemberToEmployeeAsync(employeeId, dto);
+                return Ok(newFamilyMember);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { Message = ex.Message });
+            }
+        }
+
+        [HttpPost("employees")] // POST /api/admin/patients/employees
+        public async Task<IActionResult> CreateEmployee([FromBody] CreateEmployeeDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Error = "Validation failed",
+                    Details = ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage)
+                });
+            }
+            try
+            {
+                var newEmp = await _employeeService.CreateEmployeeAsync(dto);
+                return CreatedAtAction(nameof(GetEmployeeById), new { id = newEmp.Id }, newEmp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Failed to create employee",
+                    Details = ex.Message
+                });
+            }
+
+        }
+
+        [HttpGet("employees/{id}")]// GET /api/admin/patients/employees/{id}
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            try
+            {
+                var dep = await _employeeService.GetEmployeeByIdAsync(id);
+                if (dep == null) return NotFound(new { Error = $"Employee with ID {id} not found" });
+                return Ok(dep);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Error = $"An error occurred while retrieving employee with ID {id}",
+                    Details = ex.Message
+                });
+            }
+        }
+
+
+
+
+
+    }
+}
